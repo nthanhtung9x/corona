1950749ff3df0abf8d0a50ab668d84f0f10b21c9
Tùng Nguyễn Thanh <tung1831998@gmail.com>
2020-04-12 11:08:52 +0700

    Updates

---
+++ precache-manifest.1e72171202de374a454b2acfc707c2b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bf5699fbc25df4e0bb6f0f46782cb2",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "7451fe774014d1f9cee4",
+    "url": "/corona/static/css/main.c634dc6a.chunk.css"
+  },
+  {
+    "revision": "5bad9b7758cbcefd58d0",
+    "url": "/corona/static/js/2.66dcf05f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona/static/js/2.66dcf05f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7451fe774014d1f9cee4",
+    "url": "/corona/static/js/main.22038221.chunk.js"
+  },
+  {
+    "revision": "20a9fba9e867ee5a083e",
+    "url": "/corona/static/js/runtime-main.e3f887a0.js"
+  },
+  {
+    "revision": "a261e3e587599bc0353eee7b635f3fab",
+    "url": "/corona/static/media/arrow.a261e3e5.svg"
+  },
+  {
+    "revision": "bcc98b5e2278e840ead06396d147b91f",
+    "url": "/corona/static/media/menu.bcc98b5e.svg"
+  }
+]);
